465fd3dc175cef715d6d480d6b4d0573691e8e13
AlvaroMYDUOC <alv.madrid@duocuc.cl>
2023-09-07 10:29:48 -0400

    Construcción de Navbar2 para prueba

--- workingit/src/App.js
+++ workingit/src/App.js
@@ -5,12 +5,14 @@ import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 
 //Importe de componentes
 import Navbar from './components/Navbar';
+import Navbar2 from './components/Navbar2';
 
 function App() {
   return (
     <Router>
           <Routes>
               <Route  path="/Navbar" element={<Navbar/>} />
+              <Route  path="/Navbar2" element={<Navbar2/>} />
           </Routes>
         </Router>
   );
---
+++ workingit/src/components/Navbar2.jsx
@@ -0,0 +1,63 @@
+import React from 'react';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import Offcanvas from 'react-bootstrap/Offcanvas';
+
+function Navbar2() {
+  return (
+    <>
+      {['lg'].map((expand) => (
+        <Navbar key={expand} expand={expand} className="bg-body-tertiary mb-3">
+          <Container fluid>
+            <Navbar.Brand href="#home">
+                <img
+                alt=""
+                src="./assets/img/icon.png"
+                width="30"
+                height="30"
+                className="d-inline-block align-top"
+                />
+            </Navbar.Brand>
+            <Navbar.Brand href="#">Working It</Navbar.Brand>
+            <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-${expand}`} />
+            <Navbar.Offcanvas
+              id={`offcanvasNavbar-expand-${expand}`}
+              aria-labelledby={`offcanvasNavbarLabel-expand-${expand}`}
+              placement="end"
+            >
+              <Offcanvas.Header closeButton>
+                <Offcanvas.Title id={`offcanvasNavbarLabel-expand-${expand}`}>
+                  Menú
+                </Offcanvas.Title>
+              </Offcanvas.Header>
+              <Offcanvas.Body>
+                <Nav className="justify-content-end flex-grow-1 pe-3">
+                <NavDropdown
+                    title="Categorías"
+                    id={`offcanvasNavbarDropdown-expand-${expand}`}>
+                    <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
+                    <NavDropdown.Item href="#action4">
+                      Another action
+                    </NavDropdown.Item>
+                    <NavDropdown.Divider />
+                    <NavDropdown.Item href="#action5">
+                      Something else here
+                    </NavDropdown.Item>
+                  </NavDropdown>
+                  <Nav.Link href="#action1">Solicita una asesoría</Nav.Link>
+                  <Nav.Link href="#action1">¿Eres un especialista?</Nav.Link>
+                  <Nav.Link href="#action2">Crea tu cuenta</Nav.Link>
+                  <Nav.Link href="#action2">Ingresar</Nav.Link>
+                </Nav>
+              </Offcanvas.Body>
+            </Navbar.Offcanvas>
+          </Container>
+        </Navbar>
+      ))}
+    </>
+  );
+}
+
+export default Navbar2;
