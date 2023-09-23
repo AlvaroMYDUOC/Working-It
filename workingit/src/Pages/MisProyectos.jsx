9a85f459f38b7c84f04f6952155e7dda1c205c47
AlvaroMYDUOC <alv.madrid@duocuc.cl>
2023-09-23 14:29:59 -0400

    Login + Navbar con token

---
+++ workingit/src/Pages/MisProyectos.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export class MisProyectos extends Component {
+  render() {
+    return (
+      <div>MisProyectos</div>
+    )
+  }
+}
+
+export default MisProyectos
