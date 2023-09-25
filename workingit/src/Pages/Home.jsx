1306794d3a2595c88c28a53b68bd9040e9b49946
AlvaroMYDUOC <alv.madrid@duocuc.cl>
2023-09-25 04:00:06 -0400

    Navbar identificador de usuarios, Fixeo en ruteo de Registor, Login que almacena datos en localStoragee

--- workingit/src/Pages/Home.jsx
+++ workingit/src/Pages/Home.jsx
@@ -2,8 +2,6 @@ import React from 'react'
 import Navbar2 from "../components/Navbar2"
 import Footer from "../components/Footer"
 import ChatBot from '../components/BotChat'
-import Carrusel from '../components/Carrusel'
-import Buscador from '../components/Buscador'
 
 const Home = () => {
   return (
