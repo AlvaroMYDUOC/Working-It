2663fa3dd419a68bc46945b09d4e017cb0213a1a
walterpcabrera <walt.pacheco@duocuc.cl>
2023-09-26 18:35:55 -0300

    Modificacion de rutas Appjs

--- workingit/src/App.js
+++ workingit/src/App.js
@@ -3,20 +3,9 @@ import './assets/css/App.css';
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 
 //Importe de componentes
-
-import Navbar2 from './components/Navbar2';
-
-import Registro from './components/Registro';
-import Proyectos from './components/Proyectos';
-import SpecialistProfile from './components/SpecialistProfile';
 import Home from './Pages/Home.jsx';
-import DataTable from './components/DataTable.jsx';
-
 import Login from './Pages/Login.jsx';
-import RegistroI from './components/RegistroI.jsx';
 import Rc from './Pages/RegistroCliente';
-import RegistroP from './components/RegistroP.jsx';
-import FooterD from './components/FooterD.jsx';
 
 
 
@@ -27,25 +16,6 @@ function App() {
               <Route  path="/" element={<Home />}/>
               <Route  path="/RegistroClientes" element={<Rc/>}/>
               <Route  path="/Login" element={<Login />}/>
-              
-              
-              
-              <Route  path="/Navbar2" element={<Navbar2/>} />
-              <Route  path="/Proyectos" element={<Proyectos/>} />
-              <Route  path="/Registro" element={<Registro/>} />
-              <Route  path="/MiPerfil" element={<SpecialistProfile/>} />
-              <Route  path="/DataTable" element={<DataTable/>} />
-
-
-              <Route  path="/RegistroI" element={<RegistroI/>} />
-        
-              <Route  path="/RegistroP" element={<RegistroP/>} />
-              <Route  path="/FooterD" element={<FooterD/>} />
-              
-              
-              
-
-
           </Routes>
         </Router>
   );
