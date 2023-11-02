26bd2face8469ccfe1354cd2eeacd150c3019442
walterpcabrera <walt.pacheco@duocuc.cl>
2023-11-02 14:02:24 -0300

    New-Chat-Component

--- workingit/src/App.js
+++ workingit/src/App.js
@@ -6,6 +6,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Registro from './components/Registro';
 import MisProyectos from './Pages/MisProyectos'
 import SpecialistProfile from './components/SpecialistProfile';
+
 //Rutas pages
 import Home from './Pages/Home.jsx';
 import Login from './Pages/Login.jsx';
@@ -14,8 +15,14 @@ import RegistroCliente from './Pages/RegistroCliente.jsx';
 import RegistroProfesional from './Pages/RegistroProfesional.jsx';
 import PerfilProfesional from './Pages/MiPerfilProfesionista.jsx';
 import DirectorioProyectos from './Pages/DirectorioProyectos';
+// import DirectorioProfesionistas from './Pages/DirectorioProfesionistas';
+import Chat from './components/Chat';
+
+
+
 
 function App() {
+
   return (
     <Router>
           <Routes>
@@ -26,12 +33,17 @@ function App() {
               <Route  path="/MisProyectos" element={<MisProyectos/>} /> 
               <Route  path="/MisProyectos" element={<MisProyectos/>} /> 
               <Route  path="/RegistroCliente" element={<RegistroCliente/>} /> 
-              <Route  path="/RegistroProfesional" element={<RegistroProfesional/>} /> 
+              <Route  path="/RegistroProfesional" element={<RegistroProfesional/>} />
+
+              
               
               
               <Route  path="/Registro" element={<Registro/>} />
               <Route  path="/MiPerfil" element={<SpecialistProfile/>} />
               <Route  path="/Login" element={<Login/>} />
+
+              <Route  path="/Chat" element={<Chat/>} />
+              
 
 
               <Route  path="/DirectorioProyectos" element={<DirectorioProyectos/>} />
@@ -41,11 +53,8 @@ function App() {
             
 
 
-
-        <
-        /Routes> <
-        /Router>
-    );
+          </Routes>
+        </Router>
+  );
 }
-
 export default App;
